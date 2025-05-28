f541626bdcab1865476089c83a9eefb9a1fcf631
Darshanm416 <idarshankm@gmail.com>
2025-05-28 04:30:36 +0000

    commiting employee-service source code

---
+++ employee-service/src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,19 @@
+// employee-service/src/main/java/com/example/service/EmployeeService.java
+package com.example.service;
+
+import com.example.model.Employee;
+
+public class EmployeeService {
+    public void printEmployee(Employee emp) {
+        System.out.println("Name: " + emp.getName() + ", ID: " + emp.getId());
+    }
+}// employee-service/src/main/java/com/example/service/EmployeeService.java
+package com.example.service;
+
+import com.example.model.Employee;
+
+public class EmployeeService {
+    public void printEmployee(Employee emp) {
+        System.out.println("Name: " + emp.getName() + ", ID: " + emp.getId());
+    }
+}
