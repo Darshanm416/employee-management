8a54f6557a1131a778f398d675bfb3dfa75f1b16
Darshanm416 <idarshankm@gmail.com>
2025-05-28 05:07:36 +0000

    error fixed

--- employee-service/src/main/java/com/example/service/EmployeeService.java
+++ employee-service/src/main/java/com/example/service/EmployeeService.java
@@ -1,13 +1,4 @@
-// employee-service/src/main/java/com/example/service/EmployeeService.java
-package com.example.service;
-
-import com.example.model.Employee;
-
-public class EmployeeService {
-    public void printEmployee(Employee emp) {
-        System.out.println("Name: " + emp.getName() + ", ID: " + emp.getId());
-    }
-}// employee-service/src/main/java/com/example/service/EmployeeService.java
+i// employee-service/src/main/java/com/example/service/EmployeeService.java
 package com.example.service;
 
 import com.example.model.Employee;
